1ec03cf66530b1604c62057d8248e8cce1dd768f
Zhaohua Liang <lzh175603@163.com>
2026-05-31 22:32:21 +0800

    chore(build): bump version to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ghealth.tools"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     signingConfigs {
