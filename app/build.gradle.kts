f84c4a7850c90600e5b55e8262efdabee60d34e3
XiaoPb <lzh175603@163.com>
2026-08-10 14:34:20 +0800

    chore(app): bump version to 0.6.32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.ghealth.tools"
         minSdk = 24
         targetSdk = 35
-        versionCode = 631
-        versionName = "0.6.31"
+        versionCode = 632
+        versionName = "0.6.32"
     }
 
     signingConfigs {
